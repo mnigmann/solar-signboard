2da79ac9fcde011d6ed302495c9937d332e79d2e
mnigmann <83934411+mnigmann@users.noreply.github.com>
2022-07-21 09:02:03 -0400

    Add feature for adjusting colors

--- convert_image.py
+++ convert_image.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy
 import sys
 import os.path
 
@@ -10,18 +11,21 @@ def bad_arguments():
           "                            with width w and height h")
     print("    scale-integer=<w>x<h>   Scale the image by an integer amount so it fits\n"
           "                            inside a w by h rectangle")
-    print("The third command line is optional and specifies a crop area to be applied\n"
-          "to the image before scaling it")
-    print("    crop=<x0>,<y0>,<x1>,<y1>    Note that any of the values may be empty to\n"
-          "                                indicate that the crop area extends to the edge")
+    print("Optional command line arguments")
+    print("    Specifies a crop area to be applied to the image before scaling it")
+    print("        crop=<x0>,<y0>,<x1>,<y1>    Note that any of the values may be empty to\n"
+          "                                    indicate that the crop area extends to the edge")
+    print("    Create #define statements for every color")
+    print("        name-colors")
     exit()
 
 if len(sys.argv) < 3: bad_arguments()
 if sys.argv[1] in "--help": bad_arguments()
 
 i = cv2.imread(sys.argv[1])
-if len(sys.argv) == 4 and sys.argv[3].startswith("crop"):
-    x0, y0, x1, y1 = sys.argv[3][5:].split(",")
+crop = [x for x in sys.argv[1:] if x.startswith("crop")]
+if crop:
+    x0, y0, x1, y1 = crop[0][5:].split(",")
     x0 = int(x0.strip()) if x0.strip() else None
     y0 = int(y0.strip()) if y0.strip() else None
     x1 = int(x1.strip()) if x1.strip() else None
@@ -49,8 +53,21 @@ if mode.startswith("scale-integer"):
 fname = os.path.basename(os.path.splitext(sys.argv[1])[0])
 print("#define IMAGE_{}_WIDTH {}".format(fname, i.shape[1]))
 print("#define IMAGE_{}_HEIGHT {}".format(fname, i.shape[0]))
-flat = i[:, :, ::-1].flatten().tolist()
-print("const uint8_t IMAGE_{}[] = {{\n    ".format(fname), end="")
-print(",\n    ".join(",".join(str(k>>5) for k in flat[3*x:3*(x+i.shape[1])]) for x in range(0, i.shape[1]*i.shape[0], i.shape[1])))
-print("\n};")
+if "name-colors" in sys.argv[1:]:
+    flat = i[:, :, ::-1].reshape((-1, 3))
+    flat = flat >> 5
+    colors = numpy.unique(flat, axis=0)
+    flat = flat.tolist()
+    colors = colors.tolist()
+    for q, x in enumerate(colors):
+        print("#define IMAGE_{}_COLOR{} {},{},{}".format(fname, q, *x))
+    print("const uint8_t IMAGE_{}[] = {{\n    ".format(fname), end="")
+    root = "IMAGE_{}_COLOR".format(fname)
+    print(",\n    ".join(",".join(root+str(colors.index(k)) for k in flat[x:x+i.shape[1]]) for x in range(0, i.shape[1]*i.shape[0], i.shape[1])))
+    print("\n};")
 
+else:
+    flat = i[:, :, ::-1].flatten().tolist()
+    print("const uint8_t IMAGE_{}[] = {{\n    ".format(fname), end="")
+    print(",\n    ".join(",".join(str(k>>5) for k in flat[3*x:3*(x+i.shape[1])]) for x in range(0, i.shape[1]*i.shape[0], i.shape[1])))
+    print("\n};")
